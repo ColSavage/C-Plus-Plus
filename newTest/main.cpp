35c460472da68a94d4f7daa79c3233a8f1e1f35d
ColSavage <htarkalson36@gmail.com>
2022-07-26 06:04:28 -0600

    New change

--- newTest/main.cpp
+++ newTest/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Can I make changes like this so that I can code a project on both computers?" << std::endl;
     return 0;
 }
